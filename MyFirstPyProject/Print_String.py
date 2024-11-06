d2353f707716b33af3bb79a7a956dc3ec7844f08
IvanDam07 <isobrinoc01@educastillalamancha.es>
2024-11-06 10:12:13 +0100

    Actualización 1.0
    -Añadidos primeros ejercicios Pycharm

---
+++ MyFirstPyProject/Print_String.py
@@ -0,0 +1,17 @@
+#print
+print("Hola mundo")
+print("Hola Mundo te digo 'Hola'")
+print(100+50)
+print("100+50")
+
+#Strings
+print("Hola"+" Marta")
+print("Me llamo \"Inma\"")
+print("Esta es una linea\nY esta es otro linea")
+print("\tEsta es la tercera linea")
+print('This isn\'t a number')
+
+#Input
+input("Dime tu nombre")
+print(input("Dime tu apellido: "))
+print("Tu nombre es " +input("Dime tu nombre: ") + " " +input("Dime tu apellido: "))
